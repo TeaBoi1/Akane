b8f85dcc10a23356f4832b34c52ddd1db9d097ca
TeaBoi1 <43237308+TeaBoi1@users.noreply.github.com>
2018-09-23 19:01:39 +0300

    Update akane.py

--- akane.py
+++ akane.py
@@ -15,7 +15,7 @@ Clientdiscord = discord.Client()
 
 @client.event
 async def on_ready():
-    await client.change_presence(game=Game(name='with Nova'))
+    await client.change_presence(game=Game(name='with my toys'))
     print('Ready') 
 
 
